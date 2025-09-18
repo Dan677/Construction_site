3132e61e2c59ed1c3a7fea079c2dcc22b65d8c6c
dan.muresan <dan.muresan@orange.com>
2025-09-18 12:28:32 +0300

    Initial commit of the construction page project

---
+++ assets/js/main.js
@@ -0,0 +1,117 @@
+$(function() {
+
+    "use strict";
+
+    //===== Prealoder
+
+    $(window).on('load', function (event) {
+        $('.preloader').delay(500).fadeOut(500);
+    });
+
+
+    //===== Sticky
+
+    $(window).on('scroll', function (event) {
+        var scroll = $(window).scrollTop();
+        if (scroll < 20) {
+            $(".header_navbar").removeClass("sticky");
+            $(".header_navbar img").attr("src", "assets/images/logo.png");
+        } else {
+            $(".header_navbar").addClass("sticky");
+            $(".header_navbar img").attr("src", "assets/images/logo-dark.png");
+        }
+    });
+
+
+    //===== Section Menu Active
+
+    var scrollLink = $('.page-scroll');
+    // Active link switching
+    $(window).scroll(function () {
+        var scrollbarLocation = $(this).scrollTop();
+
+        scrollLink.each(function () {
+
+            var sectionOffset = $(this.hash).offset().top - 73;
+
+            if (sectionOffset <= scrollbarLocation) {
+                $(this).parent().addClass('active');
+                $(this).parent().siblings().removeClass('active');
+            }
+        });
+    });
+    
+
+    //===== close navbar-collapse when a  clicked
+
+    $(".navbar-nav a").on('click', function () {
+        $(".navbar-collapse").removeClass("show");
+    });
+
+    $(".navbar-toggler").on('click', function () {
+        $(this).toggleClass("active");
+    });
+
+    $(".navbar-nav a").on('click', function () {
+        $(".navbar-toggler").removeClass('active');
+    });
+    
+
+    //===== Back to top
+
+    // Show or hide the sticky footer button
+    $(window).on('scroll', function (event) {
+        if ($(this).scrollTop() > 600) {
+            $('.back-to-top').fadeIn(200)
+        } else {
+            $('.back-to-top').fadeOut(200)
+        }
+    });
+
+
+    //Animate the scroll to yop
+    $('.back-to-top').on('click', function (event) {
+        event.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: 0,
+        }, 1500);
+    });
+
+
+    //=====  WOW active
+
+    var wow = new WOW({
+        boxClass: 'wow', //
+        mobile: false, // 
+    })
+    wow.init();
+
+    //===== 
+const darkModeToggle = document.getElementById('darkModeToggle');
+const body = document.body;
+const icon = document.getElementById('icon');
+
+// Verifică dacă dark mode este activat dinainte și setează stilul corespunzător
+if (localStorage.getItem('darkMode') === 'enabled') {
+    body.classList.add('dark-mode');
+    icon.classList.remove('fa-moon');
+    icon.classList.add('fa-sun'); // Schimbă icoana la soare pentru light mode
+}
+
+// Schimbă între dark mode și light mode la click
+darkModeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+
+    // Schimbă icon-ul în funcție de dark mode
+    if (body.classList.contains('dark-mode')) {
+        icon.classList.remove('fa-sun');
+        icon.classList.add('fa-moon'); // Schimbă la lună pentru dark mode
+        localStorage.setItem('darkMode', 'enabled');
+    } else {
+        icon.classList.remove('fa-moon');
+        icon.classList.add('fa-sun'); // Schimbă la soare pentru light mode
+        localStorage.setItem('darkMode', 'disabled');
+    }
+});
+});
